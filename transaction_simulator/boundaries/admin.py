f9b6eaa0ff0a9f92a5515e3a89871106f51f72cb
Dheeraj Chand <dheeraj.chand@gmail.com>
2017-02-12 03:58:45 -0600

    Trying to clean repository; big commit, this may take hours
    Former-commit-id: 7bc3d61306bd84ea6350cb33ba1bbc0f24b18b79

--- transaction_simulator/boundaries/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis import admin
-from .models import Country, Admin_Level_5, Admin_Level_4, Admin_Level_3, Admin_Level_2, Admin_Level_1
-
-admin.site.register(Country, admin.OSMGeoAdmin)
-# admin.site.register(Admin_Level_5, admin.OSMGeoAdmin)
-# admin.site.register(Admin_Level_4, admin.OSMGeoAdmin)
-# admin.site.register(Admin_Level_3, admin.OSMGeoAdmin)
-# admin.site.register(Admin_Level_2, admin.OSMGeoAdmin)
-# admin.site.register(Admin_Level_1, admin.OSMGeoAdmin)
